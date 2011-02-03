32e6a763eb974d464717a8a4295af4795aa1a9f5
Wilhelm Murdoch <wilhelm.murdoch@gmail.com>
2011-02-04 01:20:10 +1100

    Outline Request process.

--- system/classes/sauce/request.php
+++ system/classes/sauce/request.php
@@ -5,21 +5,26 @@
 		static private $permittedHttpMethods = array (
 			'GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'
 		);
+
 		public function __construct() {
 
 		}
+
 		static public function factory() {
 			return new self;
 		}
+
 		static public function singleton() {
 			if((self::$instance instanceof self) === false) {
 				return self::$instance = new self;
 			}
 			return self::$instance;
 		}
+
 		private function __call($method, $parameters) {
 			return $this;
 		}
+		
 		public function execute() {
 
 		}
